96a8ff323ee3918bb6771fec865a3a2d064a602c
Etienne TOVIMAFA <phenixdark27@gmail.com>
2022-03-24 18:34:28 +0100

    Update server.py

--- server.py
+++ server.py
@@ -28,11 +28,8 @@ def write_to_csv(data):
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        try:
             data = request.form.to_dict()
             write_to_csv(data)
             return redirect('thank.html')
-        except:
-            return "Did not save to database"
     else:
         return'Something went wrong'
